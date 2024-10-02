165a756b150ce2f424ab0b0f0ada8b739a992aec
Silvana Ruiz Medina <146529802+SilvanaRuiz@users.noreply.github.com>
2024-10-02 10:16:20 +0200

    Update limpieza.py

--- limpieza.py
+++ limpieza.py
@@ -3,22 +3,37 @@ def clean_airbnb_data(df):
     df = df.drop(columns='Unnamed: 0')
     
     #Convertir 'rating' a float
-    
     df.replace({'rating':'reviews'}, np.nan, inplace=True)
     df['rating'] = pd.to_numeric(df['rating'], errors='coerce')
-    #Extraemos el currency
+
+    #Extraemos el currency y creamos nueva columna
     df['currency'] = df['price'].str.extract(r'([^\d.,]+)')
     #Limpiar y convertir la columna 'price' en valores numéricos
     df['price'] = df['price'].str.replace(r'[^\d.,]', '', regex=True)
     df['price'] = pd.to_numeric(df['price'].str.replace(',', ''), errors='coerce')
+
+    #Convertir el hosting time a meses, para tener una medida uniforme
+    def convert_to_months(time_str):
+        if 'year' in time_str:
+            years = int(re.findall(r'(\d+)', time_str)[0])
+            months = years * 12
+        elif 'month' in time_str:
+            months = int(re.findall(r'(\d+)', time_str)[0])
+        else:
+            months = 0
+        return months
     
-    #Extraer el número de años de 'hosting_time'
-    df['hosting_time'] = df['hosting_time'].str.extract(r'(\d+)').astype(float)
+    df['hosting_time']= df['hosting_time'].apply(convert_to_months)
     
     #'type_host' a categoría y eliminamos lo de bathroom
     df['type_host'] = df['type_host'].str.replace(r' · (Shared bathroom|Shared half bathroom|No bathroom)', '', regex=True)
     df.replace({'type_host':''}, np.nan, inplace=True)
     df.replace({'type_host':np.nan}, 'no_category', inplace=True)
+
+    #Por problemas de scrapping hay datos que no hemos podido obtener pero obtendremos, por el momento eliminaremos las colundas con Nan
+    for i, row in df.iterrows():
+        if row.isnull().any(): 
+            df.drop(i, inplace=True)
    
     #Reordenar columnas
     column_order = ['guest_favorite', 'rating', 'number_reviews', 'type_host', 
